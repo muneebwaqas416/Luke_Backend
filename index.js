bcadd7418a2b4e4af10659ed4515bbc772313fc9
MUNEEB WAQAS <muneebwaqas416@gmail.com>
2024-07-18 22:17:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,19 +11,10 @@ const router = require('./Routers/generateDayRouter');
 // Middleware to parse JSON bodies
 
 app.use(express.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "full-primal-ai.web.app"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use(cors(
-    {
-        origin:'http://localhost:5173', 
-        credentials:true,            //access-control-allow-credentials:true
-        optionSuccessStatus:200
-    } 
-)); // If you need to handle CORS
-
+app.use(cors({
+  origin : true,
+  credentials : true
+}));
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 })
