ac708d60ed55a3b29e48fca583668e9b35b86a7f
MUNEEB WAQAS <muneebwaqas416@gmail.com>
2024-07-18 22:01:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(cors(
     {
         origin : "http://localhost:5173",
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials : true 
     }
 )); // If you need to handle CORS
